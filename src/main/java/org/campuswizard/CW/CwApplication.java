526d5596065011bc8d70d30f72668e3c7193c7ae
Sampath <nupa.sampath2@gmail.com>
2019-12-25 22:45:33 +0530

    First DB Hit

--- src/main/java/org/campuswizard/CW/CwApplication.java
+++ src/main/java/org/campuswizard/CW/CwApplication.java
@@ -1,12 +1,13 @@
 package org.campuswizard.CW;
 
-import org.campuswizard.controller.LoginController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = LoginController.class)
+@EnableMongoRepositories({"org.campuswizard.repos"})
+@ComponentScan({"org.campuswizard.controller","org.campuswizard.model"})
 public class CwApplication {
 
 	public static void main(String[] args) {
--- src/main/java/org/campuswizard/controller/LoginController.java
+++ src/main/java/org/campuswizard/controller/LoginController.java
@@ -1,11 +1,16 @@
 package org.campuswizard.controller;
 
-import org.springframework.http.HttpRequest;
+import org.campuswizard.model.Employees;
+import org.campuswizard.repos.EmployeesRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.EntityResponse;
 
 
 
@@ -13,13 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
 	
+	@Autowired EmployeesRepo employeesRepo;
+	
 	@RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json")
-    public boolean getLogin(@RequestBody testBean request) {
+    public ResponseEntity<?> getLogin(@RequestBody Employees request) {
 		
-System.out.println("its going in ");
-		System.out.println(request.getUsername());
+		Employees empResult=employeesRepo.findByEmpidAndPassword(request.getEmpid(), request.getPassword());
 		
-      return true;  
+		if(empResult != null) {
+			return new ResponseEntity<>(empResult, HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<>("Bad Credentials", HttpStatus.BAD_REQUEST);
+		}
     }
 
 }
